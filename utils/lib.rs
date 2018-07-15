d4bbdf0bbf8fadece5cbb3ed91e8fade22144572
Jacky Zhen <jacky.s.zhen@gmail.com>
2018-07-15 20:32:34 +1200

    Use a deterministic seed for rand test

--- utils/lib.rs
+++ utils/lib.rs
@@ -3,57 +3,66 @@
 extern crate lazy_static;
 extern crate rand;
 
-use rand::Rng;
+use rand::{ Rng, StdRng, SeedableRng};
 use std::iter;
 
 const LARGE_KEYS: usize = 100_000_000;
 const SMALL_KEYS: usize = 50;
+const RNG_SEED: [u8;32]  = [
+    1, 2, 3, 4,
+    5, 6, 7, 9,
+    1, 2, 3, 9,
+    4, 2, 6, 9,
+    4, 7, 1, 0,
+    4, 2, 1, 0,
+    4, 9, 8, 5,
+    8, 9, 8, 5];
 
 lazy_static! {
     pub static ref RANDOM_100_000: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..100_000).map(|_| {
             rng.gen_range(0, LARGE_KEYS)
         }).collect()
     };
     pub static ref RANDOM_10_000: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..10_000).map(|_| {
             rng.gen_range(0, LARGE_KEYS)
         }).collect()
     };
     pub static ref RANDOM_1000: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..1000).map(|_| {
             rng.gen_range(0, LARGE_KEYS)
         }).collect()
     };
     pub static ref RANDOM_100: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..100).map(|_| {
             rng.gen_range(0, LARGE_KEYS)
         }).collect()
     };
     pub static ref RANDOM_100_000_FEW_KEYS: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..100_000).map(|_| {
             rng.gen_range(0, SMALL_KEYS)
         }).collect()
     };
     pub static ref RANDOM_10_000_FEW_KEYS: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..10_000).map(|_| {
             rng.gen_range(0, SMALL_KEYS)
         }).collect()
     };
     pub static ref RANDOM_1000_FEW_KEYS: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..1000).map(|_| {
             rng.gen_range(0, SMALL_KEYS)
         }).collect()
     };
     pub static ref RANDOM_100_FEW_KEYS: Vec<usize> = {
-        let mut rng = rand::thread_rng();
+        let mut rng: StdRng = StdRng::from_seed(RNG_SEED);
         (0..100).map(|_| {
             rng.gen_range(0, SMALL_KEYS)
         }).collect()
